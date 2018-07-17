8129a1f87ababc66110f11495896b82720e1f9b5
Kapil Mohan <kapilonweb@gmail.com>
2018-07-17 17:30:59 +0530

    nrwl/nx based code structure

---
+++ apps/mapledesign-e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('sweetsugar-root h1')).getText();
+  }
+}
